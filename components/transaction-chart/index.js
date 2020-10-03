146e47f4e013b723d4e5bc8c099892b34879e9ba
Harshith Venkatesh <44545218+Harshi7016@users.noreply.github.com>
2020-10-04 00:48:10 +0530

    New MockData changes and data display

--- components/transaction-chart/index.js
+++ components/transaction-chart/index.js
@@ -2,35 +2,46 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { useState, useEffect } from 'react/cjs/react.development';
 import styles from './transaction-chart.module.css';
+
+function getDataset(transactionData) {
+  let dataLabels = [];
+  let creditData = [];
+  let debitData = [];
+  for (let data of transactionData) {
+    if (data.type === 'credit') {
+      dataLabels.push(data.timeStamp);
+      creditData.push(data.amount);
+    } else {
+      debitData.push(data.amount);
+    }
+  }
+  return {
+    labels: dataLabels,
+    datasets: [
+      {
+        label: 'Credit',
+        fill: false,
+        data: creditData,
+        borderColor: ['rgba(42,187,155,0.9)'],
+        pointBorderColor: ['rgba(42,187,155,0.9)'],
+        borderWidth: 4
+      },
+      {
+        label: 'Debit',
+        fill: false,
+        data: debitData,
+        borderColor: ['rgba(255,0,0,06)'],
+        pointBorderColor: ['rgba(255,0,0,0.6)'],
+        borderWidth: 4
+      }
+    ]
+  };
+}
+
 const TransactionChart = (props) => {
-  const {
-    transactionDates,
-    creditData,
-    debitData
-  } = props.transactionChartData;
   const [chartData, setChartData] = useState({});
   const chart = () => {
-    setChartData({
-      labels: transactionDates,
-      datasets: [
-        {
-          label: 'Credit',
-          fill: false,
-          data: creditData,
-          borderColor: ['rgba(42,187,155,0.9)'],
-          pointBorderColor: ['rgba(42,187,155,0.9)'],
-          borderWidth: 4
-        },
-        {
-          label: 'Debit',
-          fill: false,
-          data: debitData,
-          borderColor: ['rgba(255,0,0,06)'],
-          pointBorderColor: ['rgba(255,0,0,0.6)'],
-          borderWidth: 4
-        }
-      ]
-    });
+    setChartData(getDataset(props.transactionChartData));
   };
 
   useEffect(() => {
