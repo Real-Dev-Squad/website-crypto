2a515d52dd0fb185ec47804c6d9cdf636f8cb0d4
Harshith Venkatesh <44545218+Harshi7016@users.noreply.github.com>
2020-10-11 18:58:21 +0530

    Transaction Graph changes added (#38)
    * Transaction Graph changes added
    
    * Changes on height and width for mobile view
    
    * Addition of css changes for responsiveness of graph
    
    * creating mockData for transaction Data
    
    * removed console.logs and unnecessary data
    
    * local git check
    
    * mockData changes as per discussion
    
    * Repo rebase
    
    * New MockData changes and data display
    
    * mockData Changes
    
    Co-authored-by: Harshith7016 <32194436+Harshith7016@users.noreply.github.com>

---
+++ components/transaction-chart/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { useState, useEffect } from 'react/cjs/react.development';
+import styles from './transaction-chart.module.css';
+
+function getDataset(transactionData) {
+  let dataLabels = [];
+  let creditData = [];
+  let debitData = [];
+  for (let eachTransaction of transactionData) {
+    dataLabels.push(eachTransaction.timeStamp);
+    if (eachTransaction.data[0].type === 'credit') {
+      creditData.push(eachTransaction.data[0].amount) || creditData.push(0);
+    }
+    if (eachTransaction.data[1].type === 'debit') {
+      debitData.push(eachTransaction.data[1].amount) || debitData.push(0);
+    }
+  }
+  return {
+    labels: dataLabels,
+    datasets: [
+      {
+        label: 'Credit',
+        fill: false,
+        data: creditData,
+        borderColor: ['rgba(42,187,155,0.9)'],
+        pointBorderColor: ['rgba(42,187,155,0.9)'],
+        borderWidth: 4
+      },
+      {
+        label: 'Debit',
+        fill: false,
+        data: debitData,
+        borderColor: ['rgba(255,0,0,06)'],
+        pointBorderColor: ['rgba(255,0,0,0.6)'],
+        borderWidth: 4
+      }
+    ]
+  };
+}
+
+const TransactionChart = (props) => {
+  const [chartData, setChartData] = useState({});
+  const chart = () => {
+    setChartData(getDataset(props.transactionChartData));
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+
+  return (
+    <div className={styles.transactionCard}>
+      <Line
+        width={500}
+        height={300}
+        data={chartData}
+        options={{
+          responsive: true,
+          maintainAspectRatio: false,
+          title: { text: 'RDS Transaction', display: true },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  autoSkip: true
+                },
+                gridLines: {
+                  display: true
+                }
+              }
+            ],
+            xAxes: [
+              {
+                gridLines: {
+                  display: true
+                }
+              }
+            ]
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default TransactionChart;
