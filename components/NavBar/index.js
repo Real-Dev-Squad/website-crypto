0c00ba747d374d89f45d06dea4362872a37e688c
Harshith7016 <harshithece7016@gmail.com>
2020-11-28 02:56:53 +0530

    navbar component and css changes

---
+++ components/NavBar/index.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './navbar.module.css';
+const NavBar = ({ personData: { photo } }) => {
+  return (
+    <div className={styles.homeHeader}>
+      <div className={styles.navBar}>
+        <img
+          className={styles.logo}
+          src="https://staging-members-rds.herokuapp.com/images/Real-Dev-Squad@1x.png"
+          alt="RealDevSquad Logo"
+        />
+
+        <div className={styles.homeLogin}>
+          <ul>
+            <li>
+              <Link href="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link href="/cart">Cart</Link>
+            </li>
+          </ul>
+          <p></p>
+          <img
+            src={photo}
+            className={styles.profilePic}
+            alt="Profile Image"
+            height="60"
+            width="60"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
