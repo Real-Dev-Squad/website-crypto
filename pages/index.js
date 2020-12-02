cf12440e3e3d57ad8c711ea87ace05d256d42e06
Gaurav chauhan <gauravchauhan2403@gmail.com>
2020-12-02 22:03:09 +0530

    Navbar update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Navbar from '../components/navbar';
 import transactionData from '../mock/transaction.json';
 import personData from '../mock/person.json';
 import transactionChartData from '../mock/transaction-graph-data.json';
@@ -12,37 +13,40 @@ import NavBar from '@components/NavBar';
 
 export default function Home() {
   return (
-    <div className={styles.homeContainer}>
-      <Head>
-        <title>Bank Dashboard</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NavBar personData={personData} />
-      <div className={styles.homeUser}>
-        <CoinsStatus coins={coinsData} />
-        <div className={`${styles.button} ${styles.greenButton}`}>Send</div>
-        <div className={`${styles.button} ${styles.redButton}`}>Receive</div>
-      </div>
-      <div className={styles.homeTransaction}>
-        <div className={styles.transactionGraph}>
-          <TransactionChart transactionChartData={transactionChartData} />
+    <div>
+      <Navbar />
+      <div className={styles.homeContainer}>
+        <Head>
+          <title>Bank Dashboard</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <NavBar personData={personData} />
+        <div className={styles.homeUser}>
+          <CoinsStatus coins={coinsData} />
+          <div className={`${styles.button} ${styles.greenButton}`}>Send</div>
+          <div className={`${styles.button} ${styles.redButton}`}>Receive</div>
         </div>
-        <div className={styles.transactionMenu}>
-          <div className={`${styles.card} ${styles.content}`}>
-            <div className={`${styles.heading}`}>
-              <img
-                src="https://www.flaticon.com/svg/static/icons/svg/810/810375.svg"
-                className="icon"
-                alt="Transaction Icon"
-              />
+        <div className={styles.homeTransaction}>
+          <div className={styles.transactionGraph}>
+            <TransactionChart transactionChartData={transactionChartData} />
+          </div>
+          <div className={styles.transactionMenu}>
+            <div className={`${styles.card} ${styles.content}`}>
+              <div className={`${styles.heading}`}>
+                <img
+                  src="https://www.flaticon.com/svg/static/icons/svg/810/810375.svg"
+                  className="icon"
+                  alt="Transaction Icon"
+                />
+              </div>
+              <TransactionList transactions={transactionData} />
             </div>
-            <TransactionList transactions={transactionData} />
           </div>
         </div>
-      </div>
-      <div className={styles.homeFooter}>
-        {' '}
-        <Footer />
+        <div className={styles.homeFooter}>
+          {' '}
+          <Footer />
+        </div>
       </div>
     </div>
   );
