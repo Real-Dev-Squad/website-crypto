d102aa8a773163dcdc6162fbca916690dea4a249
kachwalUttamsharma <kachwaluttamsharma@Kachwals-MacBook-Pro.local>
2021-01-30 13:53:53 +0530

    popup fix for transaction filter

--- components/filter/index.js
+++ components/filter/index.js
@@ -1,35 +1,45 @@
 import styles from './filter.module.css';
 import Image from 'next/image';
+import { useState, useRef } from 'react';
+import GenericClosePopUp from '../Close-popup/GenericClosePopUp';
 
 const Filter = (props) => {
   const { changeTransactions } = props;
+  const [toggle, setToggle] = useState(false);
+  const ref = useRef();
+  GenericClosePopUp(ref, () => {
+    setToggle(false);
+  });
   return (
-    <div className={styles.filter}>
+    <div
+      className={styles.filter}
+      onClick={() => {
+        setToggle(!toggle);
+      }}
+      ref={ref}
+    >
       <div className="icon">
         <Image
           src="/assets/filter-icon.svg"
           alt="Filter icon"
           width={20}
           height={20}
-          onClick={() => {
-            document
-              .getElementById('filterContent')
-              .classList.toggle(styles.showList);
-          }}
         />
-      </div>
-
-      <div className={styles.filterContent} id="filterContent">
-        <li onClick={() => changeTransactions({ type: 'credit' })}>
-          Sort on credit
-        </li>
-        <li onClick={() => changeTransactions({ type: 'debit' })}>
-          Sort on debit
-        </li>
-        <li>Sort on date</li>
-        <li onClick={() => changeTransactions({ type: 'reset' })}>
-          Show original data
-        </li>
+        <div
+          className={toggle ? styles.showList : styles.hideList}
+          id="filterContent"
+        >
+          <li onClick={() => changeTransactions({ type: 'credit' })}>
+            Sort on credit
+          </li>
+          <li onClick={() => changeTransactions({ type: 'debit' })}>
+            Sort on debit
+          </li>
+          <li>Sort on date</li>
+          <li onClick={() => changeTransactions({ type: 'reset' })}>
+            Show original data
+          </li>
+        </div>
       </div>
     </div>
   );
