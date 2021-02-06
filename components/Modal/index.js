999347d944019bb051283ad994537b014f15f274
RohitKuwar <rohit1kuwar@gmail.com>
2021-02-06 23:27:57 +0530

    Added modal component

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,11 +1,46 @@
-import PropTypes from 'prop-types';
+import React, { useRef, useCallback, useEffect } from 'react';
+import classNames from './modal.module.css';
 
-const Modal = () => {
-  return <div> </div>;
-};
+export default function Modal({ showModal, setShowModal, render }) {
+  const modalRef = useRef();
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
 
-Modal.propTypes = {
-  name: PropTypes.string,
-};
-
-export default Modal;
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+    return () => document.removeEventListener('keydown', keyPress);
+  }, [keyPress]);
+  return (
+    <>
+      {showModal ? (
+        <div
+          className={classNames.background}
+          onClick={closeModal}
+          ref={modalRef}
+        >
+          <div className={classNames.container}>
+            <span
+              className={classNames.cancelBtn}
+              button
+              onClick={() => setShowModal((prev) => !prev)}
+            >
+              &times;
+            </span>
+            <span className={classNames.subDiv}>{render}</span>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
