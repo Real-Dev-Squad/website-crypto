8fd095b0c090fad16be431bd03712cca0bdd9da7
Akashdeep-Patra <adeep8961@gmail.com>
2021-02-06 16:52:29 +0530

    utils and constants added

---
+++ utils/request/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import BASE_URL from '../../constants';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 1000,
+});
+
+export const makeRequest = (obj) => {
+  const {
+    method,
+    url,
+    responseType,
+    headers = {},
+    data = {},
+    params = {},
+  } = obj;
+  const paramString = new URLSearchParams(params);
+  try {
+    const response = axiosInstance.request(url + paramString, data, {
+      responseType,
+      method,
+      headers,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
