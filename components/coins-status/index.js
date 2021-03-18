dbce4cba0473b055c7ba84f7b75ea5c42affd0ae
Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
2021-03-18 23:53:49 +0530

    [feature/user-balance] destructuring, json method

--- components/coins-status/index.js
+++ components/coins-status/index.js
@@ -7,24 +7,25 @@ const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 const WALLET_URL = `${BASE_API_URL}/wallet`;
 
 const Coins = (props) => {
-  const [coins, changeCoins] = useAnimateValue(0, props.balance, 5000);
+  const { coin, balance } = props;
+  const [coins, changeCoins] = useAnimateValue(0, balance, 5000);
 
   useEffect(() => {
-    changeCoins(props.balance);
-  }, [props.balance, changeCoins]);
+    changeCoins(balance);
+  }, [balance, changeCoins]);
 
   return (
     <div className={styles.cointypeIndicator}>
       <div className={styles.coinData}>
-        <p> {props.coin}</p>
-        <p> {props.balance} </p>
+        <p> {coin}</p>
+        <p> {balance} </p>
       </div>
       <div className={`coin ${styles.rotateVertCenter}`}></div>
       <style jsx>{`
         .coin {
-          background-color: ${props.coin === 'neelam'
+          background-color: ${coin === 'neelam'
             ? 'blue'
-            : props.coin === 'dinero'
+            : coin === 'dinero'
             ? 'green'
             : 'gray'};
           width: calc(5em / 2);
@@ -32,18 +33,14 @@ const Coins = (props) => {
           border-radius: 50%;
           box-shadow: 3px 3px 3px #000;
           border: 1px solid
-            ${props.coin === 'neelam'
-              ? 'blue'
-              : props.coin === 'dinero'
-              ? 'green'
-              : 'gray'};
+            ${coin === 'neelam' ? 'blue' : coin === 'dinero' ? 'green' : 'gray'};
         }
       `}</style>
     </div>
   );
 };
 
-const CoinsStatus = (props) => {
+const CoinsStatus = () => {
   const [currencies, setCurrencies] = useState();
 
   useEffect(async () => {
@@ -53,8 +50,8 @@ const CoinsStatus = (props) => {
       coinsArray = [];
 
     walletData = await fetch(WALLET_URL, { credentials: 'include' });
-    walletJson = await walletData.text();
-    currencyBalance = JSON.parse(walletJson).wallet.currencies;
+    walletJson = await walletData.json();
+    currencyBalance = walletJson.wallet.currencies;
     for (const name in currencyBalance) {
       coinsArray.push(
         <Coins coin={name} balance={currencyBalance[name]} key={name} />
