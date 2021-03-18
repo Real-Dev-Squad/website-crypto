7b619fc39fa5dbfbd1e5959b480042502f171d06
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-03-18 15:32:54 +0530

    Modifying buy and sell stock cards

--- components/stock-operation/index.js
+++ components/stock-operation/index.js
@@ -1,6 +1,7 @@
 import StockOperationModal from '@components/stock-operation-modal';
 import React, { useState } from 'react';
 import styles from '../../styles/Home.module.css';
+import { useRouter } from 'next/router';
 
 const StockOperation = (props) => {
   const { id, name, price } = props.stock;
@@ -11,20 +12,26 @@ const StockOperation = (props) => {
     showModal((prev) => !prev);
   };
 
+  const router = useRouter();
+
   return (
     <div className={styles.buttonWrapper}>
-      <div
-        className={`${styles.button} ${styles.greenButton}`}
-        onClick={() => setTransaction('BUY')}
-      >
-        BUY
-      </div>
-      <div
-        className={`${styles.button} ${styles.redButton}`}
-        onClick={() => setTransaction('SELL')}
-      >
-        SELL
-      </div>
+      {router.pathname == '/trading' && (
+        <div
+          className={`${styles.button} ${styles.greenButton}`}
+          onClick={() => setTransaction('BUY')}
+        >
+          BUY
+        </div>
+      )}
+      {router.pathname == '/trading/sell' && (
+        <div
+          className={`${styles.button} ${styles.redButton}`}
+          onClick={() => setTransaction('SELL')}
+        >
+          SELL
+        </div>
+      )}
       <StockOperationModal
         nameOfStock={name}
         listedPriceOfStock={price}
