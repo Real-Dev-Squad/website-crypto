b18bebdee56fb9804ea51e30fd4f9e085bead027
Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
2021-03-20 12:10:14 +0530

    [feature/user-balance] fetch error handling, DRY, separation of concern

--- components/coins-status/index.js
+++ components/coins-status/index.js
@@ -5,10 +5,20 @@ import PropTypes from 'prop-types';
 
 const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 const WALLET_URL = `${BASE_API_URL}/wallet`;
+const CURRENCIES = {
+  NEELAM: 'neelam',
+  DINERO: 'dinero',
+};
 
 const Coins = (props) => {
   const { coin, balance } = props;
   const [coins, changeCoins] = useAnimateValue(0, balance, 5000);
+  const currencyColor =
+    coin === CURRENCIES.NEELAM
+      ? styles.blue
+      : coin === CURRENCIES.DINERO
+      ? styles.green
+      : styles.gray;
 
   useEffect(() => {
     changeCoins(balance);
@@ -17,23 +27,18 @@ const Coins = (props) => {
   return (
     <div className={styles.cointypeIndicator}>
       <div className={styles.coinData}>
-        <p> {coin}</p>
-        <p> {balance} </p>
+        <p>{coin}</p>
+        <p>{balance}</p>
       </div>
       <div className={`coin ${styles.rotateVertCenter}`}></div>
       <style jsx>{`
         .coin {
-          background-color: ${coin === 'neelam'
-            ? 'blue'
-            : coin === 'dinero'
-            ? 'green'
-            : 'gray'};
+          background-color: ${currencyColor};
           width: calc(5em / 2);
           height: calc(5em / 2);
           border-radius: 50%;
           box-shadow: 3px 3px 3px #000;
-          border: 1px solid
-            ${coin === 'neelam' ? 'blue' : coin === 'dinero' ? 'green' : 'gray'};
+          border: 1px solid ${currencyColor};
         }
       `}</style>
     </div>
@@ -41,30 +46,43 @@ const Coins = (props) => {
 };
 
 const CoinsStatus = () => {
-  const [currencies, setCurrencies] = useState();
+  const [currencies, setCurrencies] = useState([]);
+  const coins = [];
 
   useEffect(async () => {
-    let walletJson,
-      walletData,
-      currencyBalance,
-      coinsArray = [];
-
-    walletData = await fetch(WALLET_URL, { credentials: 'include' });
-    walletJson = await walletData.json();
-    currencyBalance = walletJson.wallet.currencies;
-    for (const name in currencyBalance) {
-      coinsArray.push(
-        <Coins coin={name} balance={currencyBalance[name]} key={name} />
-      );
-    }
-    setCurrencies(coinsArray);
+    const walletData = await fetch(WALLET_URL, { credentials: 'include' })
+      .then((response) => {
+        if (response.status >= 400 && response.status < 600) {
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      })
+      .then((ResponseJson) => {
+        const currencyBalance = ResponseJson.wallet.currencies;
+        for (const name in currencyBalance) {
+          coins.push({
+            name,
+            balance: currencyBalance[name],
+          });
+        }
+        setCurrencies(coins);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
-  return <div className={styles.coinsContainer}>{currencies}</div>;
+  return (
+    <div className={styles.coinsContainer}>
+      {currencies.map((coin) => (
+        <Coins coin={coin.name} balance={coin.balance} key={coin.name} />
+      ))}
+    </div>
+  );
 };
 
 Coins.propTypes = {
-  coin: PropTypes.object,
+  coin: PropTypes.string,
   value: PropTypes.number,
   name: PropTypes.string,
   color: PropTypes.string,
