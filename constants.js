86973ecb5a3e387b81983364d55d1dabe9369dca
Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
2021-03-21 20:05:16 +0530

    [featuer/user-balance] development env specific line removed

--- constants.js
+++ constants.js
@@ -1,8 +1,7 @@
 const BASE_IMAGE_URL =
   'https://raw.githubusercontent.com/Real-Dev-Squad/website-static/main/members';
 
-const BASE_API_URL = 'http://localhost:3001';
-// const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 const WALLET_URL = `${BASE_API_URL}/wallet`;
 const CURRENCIES = {
   NEELAM: 'neelam',
