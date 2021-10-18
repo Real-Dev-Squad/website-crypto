ecf048c7ed75919fee0529539011654dfa785e2c
Shubham <yadav105shubham@gmail.com>
2021-10-18 21:19:07 +0530

    Changes in navbar UI and content

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -3,9 +3,11 @@ import React, { useState, useRef } from 'react';
 import styles from './navbar.module.css';
 import Image from 'next/image';
 import GenericClosePopUp from '../Close-popup/GenericClosePopUp';
+import { LOGIN_URL } from 'constants.js';
 
 const NavBar = ({ personData: { photo } }) => {
   const RDSLogo = '/assets/Real-Dev-Squad1x.png';
+  const GITHUB_LOGO = '/assets/github.png';
   const [toggle, setToggle] = useState(false);
   const navbarRef = useRef();
   GenericClosePopUp(navbarRef, () => {
@@ -16,22 +18,58 @@ const NavBar = ({ personData: { photo } }) => {
     <nav className={styles.navBar}>
       <ul>
         <li>
-          <div className={styles.logo}>
-            <Link href="https://realdevsquad.com">
-              <Image src={RDSLogo} alt="Navbar Logo" width="50" height="50" />
-            </Link>
-          </div>
-        </li>
-        {/* <li>
-          <Link href="/shop">
-            <a>Shop</a>
+          <Link href="https://realdevsquad.com">
+            <a className={styles.logo}>
+              <Image
+                width="45px"
+                height="45px"
+                src={RDSLogo}
+                alt="real-dev squad"
+              />
+            </a>
           </Link>
         </li>
         <li>
-          <Link href="/cart">
-            <a>Cart</a>
+          <Link href="https://welcome.realdevsquad.com/">
+            <a>Welcome</a>
           </Link>
-        </li> */}
+        </li>
+        <li>
+          <Link href="https://www.realdevsquad.com/events.html">
+            <a>Events</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://members.realdevsquad.com/">
+            <a>Members</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://crypto.realdevsquad.com/">
+            <a className={styles.active}>Crypto</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://status.realdevsquad.com/">
+            <a>Status</a>
+          </Link>
+        </li>
+        <li>
+          <a className={styles.btnLogin} href={LOGIN_URL}>
+            <button className={styles.loginBtnText}>
+              Sign In With Github
+              <img
+                className={styles.githubLogo}
+                src={GITHUB_LOGO}
+                alt="Github Icon"
+              />
+            </button>
+          </a>
+          <div className={styles.userGreet}>
+            <div className={styles.userGreetMsg}>Hello text</div>
+            <img className={styles.userProfilePic} />
+          </div>
+        </li>
         <li>
           <Link href="/auction">
             <a>Auction</a>
