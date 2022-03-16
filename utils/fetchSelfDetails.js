0754c3a5792e41537beabb3c56d35024122cc2e8
Aryan <manglaaryan178@gmail.com>
2022-03-16 12:33:09 +0530

    memoised function to prevent multiple api calls

--- utils/fetchSelfDetails.js
+++ utils/fetchSelfDetails.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import fetchData from './fetchData';
 
 const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
 const fetchSelfDetails = async () => {
+  const [apiResponse, setApiResponse] = useState(undefined);
+  if (apiResponse) {
+    return apiResponse;
+  }
   const userData = await fetchData(`${BASE_API_URL}/users/self`, 'GET', {
     credentials: 'include',
   });
+  setApiResponse(userData);
   return userData;
 };
 
