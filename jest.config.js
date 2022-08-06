3da3f6cab0609db785e532d883266c0827130dcf
Vinayak <vinayak20029@gmail.com>
2022-08-06 20:42:40 +0530

    jest config updated and checked

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,24 @@ module.exports = {
       lines: 10,
     },
   },
+  moduleNameMapper: {
+    /* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+    '^.+\\.module\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    /* Handle image imports
+    https://jestjs.io/docs/webpack#handling-static-assets */
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+
+    // Handle absolute imports
+    '^@components/(.*)': '<rootDir>/components/$1',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
