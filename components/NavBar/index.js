9feaa96cce7e36d56a62fbbb933a41a870eed417
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2022-12-07 01:06:18 +0530

    Feature/integrating-new-signup-flow (#293)
    feat: integrating signup flow

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -17,6 +17,12 @@ const NavBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [mountedComponent, setMountedComponent] = useState(false);
   const navbarRef = useRef();
+
+  let authUrl = LOGIN_URL;
+  if (typeof window !== 'undefined') {
+    authUrl = `${LOGIN_URL}&state=${window.location.href}`;
+  }
+
   GenericClosePopUp(navbarRef, () => {
     setToggle(false);
   });
@@ -68,7 +74,7 @@ const NavBar = () => {
             mountedComponent ? '' : 'd-none'
           }`}
         >
-          <Link href={LOGIN_URL}>
+          <Link href={authUrl}>
             <a className={`${styles.btnLogin} ${isLoggedIn ? 'd-none' : ''}`}>
               <button className={styles.btnLoginText}>
                 Sign In
@@ -137,7 +143,7 @@ const NavBar = () => {
               mountedComponent ? '' : 'd-none'
             }`}
           >
-            <Link href={LOGIN_URL}>
+            <Link href={authUrl}>
               <a className={`${styles.btnLogin} ${isLoggedIn ? 'd-none' : ''}`}>
                 <button className={styles.btnLoginText}>
                   Sign In With GitHub
