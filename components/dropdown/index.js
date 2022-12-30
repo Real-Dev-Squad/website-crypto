c00a7dafe1b6fa3a454b46035ab7f15446d53a4d
kotesh-arya <koteswarraomudila@gmail.com>
2022-12-30 16:55:34 +0530

    UPDATE: cypress latest version

--- components/dropdown/index.js
+++ components/dropdown/index.js
@@ -19,58 +19,6 @@ const Dropdown = () => {
           <p className={styles.signoutOption}>Sign out</p>
         </li>
       </ul>
-      {/* <style jsx>
-        {`
-          .dropdown {
-            width: 180px;
-            background-color: ${colors.white};
-            border: 2px solid ${colors.black};
-            border-radius: 0.5rem;
-            color: ${colors.black};
-            position: absolute;
-            top: 58px;
-            right: 16px;
-            z-index: 100;
-          }
-
-          .dropdownList {
-            display: flex;
-            flex-direction: column;
-          }
-
-          .profileLink {
-            color: ${colors.black};
-            padding: 1rem;
-            text-align: left;
-            font-weight: 100;
-          }
-          .profileLink:hover {
-            background-color: ${colors.grey.light};
-            border-radius: 0.5rem 0.5rem 0 0;
-          }
-
-          .line {
-            border: none;
-            border-bottom: 2px solid ${colors.black};
-            margin: 0;
-          }
-
-          .dropdownItem {
-            list-style-type: none;
-            cursor: pointer;
-          }
-
-          .signoutOption {
-            padding: 1rem;
-            text-align: left;
-            font-weight: 100;
-          }
-          .signoutOption:hover {
-            background-color: ${colors.grey.light};
-            border-radius: 0 0 0.5rem 0.5rem;
-          }
-        `}
-      </style> */}
     </div>
   );
 };
