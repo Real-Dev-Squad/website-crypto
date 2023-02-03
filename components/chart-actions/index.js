8aa4c738970425479649de874c6b2cf2808e71c4
Manish Devrani <manishdevrani777@gmail.com>
2023-02-03 21:36:29 +0530

    added remaining months in month dropdown

--- components/chart-actions/index.js
+++ components/chart-actions/index.js
@@ -1,5 +1,20 @@
 import styles from './chartActions.module.css';
 
+const monthsList = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'Sepetember',
+  'October',
+  'November',
+  'December',
+];
+
 const ChartActions = () => {
   return (
     <div className={styles.chart_actions}>
@@ -8,7 +23,13 @@ const ChartActions = () => {
           Select month
         </label>
         <select id="month" className={styles.month_select}>
-          <option value="January">January</option>
+          {monthsList.map((month) => {
+            return (
+              <option key={month} value={month}>
+                {month}
+              </option>
+            );
+          })}
         </select>
       </section>
       <button className={styles.button}>Latest Transaction</button>
